61e9ff87d10e7d2359da1f6352d9f2e9ef84c405
jaziellramirez-beep <jaziellramirez@gmail.com>
2026-08-30 23:42:46 -0500

    Assignment
    the code for the conversion

--- main (3).cpp
+++ main (3).cpp
@@ -1,9 +1,11 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+// Name:Jaziel Ramirez
+// Date: september 2, 2026
+// Class: 1470.04
+// Semester: Fall 2026
+// CSCI 1470 Instructor: Dr. Reyes
+//
+// This program will convert celsius into farenheit
 
 *******************************************************************************/
 #include <iostream>
